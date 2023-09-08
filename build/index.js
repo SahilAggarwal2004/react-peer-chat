6f166bef4bf6ef98967c0463f344e7099979adee
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2023-09-08 14:26:33 +0530

    v0.1.6 bug fix

--- build/index.js
+++ build/index.js
@@ -1,19 +1,36 @@
+var __read = (this && this.__read) || function (o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+};
 import React, { useEffect, useRef } from 'react';
 import useStorage from './useStorage';
 import { BsFillMicFill, BsFillMicMuteFill } from './icons';
-export default function Chat({ peerId, remotePeerId, peerOptions, onError = () => console.error("Can not access microphone!") }) {
-    const [audio, setAudio] = useStorage('rpc-audio', false, { local: true, save: true });
-    const streamRef = useRef(null);
-    const localStream = useRef();
-    const handleRemoteStream = (remoteStream) => streamRef.current.srcObject = remoteStream;
-    useEffect(() => {
+export default function Chat(_a) {
+    var peerId = _a.peerId, remotePeerId = _a.remotePeerId, peerOptions = _a.peerOptions, _b = _a.onError, onError = _b === void 0 ? function () { return console.error("Can not access microphone!"); } : _b;
+    var _c = __read(useStorage('rpc-audio', false, { local: true, save: true }), 2), audio = _c[0], setAudio = _c[1];
+    var streamRef = useRef(null);
+    var localStream = useRef();
+    var handleRemoteStream = function (remoteStream) { return streamRef.current.srcObject = remoteStream; };
+    useEffect(function () {
         if (!audio)
             return;
-        const Peer = require('peerjs').default;
-        const peer = new Peer(`rpc-${peerId}`, peerOptions);
-        let call;
-        peer.on('open', () => {
-            const getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+        var Peer = require('peerjs').default;
+        var peer = new Peer("rpc-".concat(peerId), peerOptions);
+        var call;
+        peer.on('open', function () {
+            var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
             getUserMedia({
                 video: false,
                 audio: {
@@ -21,12 +38,12 @@ export default function Chat({ peerId, remotePeerId, peerOptions, onError = () =
                     noiseSuppression: true,
                     echoCancellation: true
                 }
-            }, (stream) => {
+            }, function (stream) {
                 localStream.current = stream;
-                call = peer.call(`rpc-${remotePeerId}`, stream);
+                call = peer.call("rpc-".concat(remotePeerId), stream);
                 call.on('stream', handleRemoteStream);
                 call.on('close', call.removeAllListeners);
-                peer.on('call', (e) => {
+                peer.on('call', function (e) {
                     call = e;
                     call.answer(stream);
                     call.on('stream', handleRemoteStream);
@@ -34,16 +51,16 @@ export default function Chat({ peerId, remotePeerId, peerOptions, onError = () =
                 });
             }, onError);
         });
-        return () => {
+        return function () {
             var _a;
-            (_a = localStream.current) === null || _a === void 0 ? void 0 : _a.getTracks().forEach(track => track.stop());
+            (_a = localStream.current) === null || _a === void 0 ? void 0 : _a.getTracks().forEach(function (track) { return track.stop(); });
             call === null || call === void 0 ? void 0 : call.removeAllListeners();
             call === null || call === void 0 ? void 0 : call.close();
             peer.removeAllListeners();
             peer.destroy();
         };
     }, [audio]);
-    return React.createElement("button", { onClick: () => setAudio(audio => !audio) }, audio ? React.createElement(React.Fragment, null,
+    return React.createElement("button", { onClick: function () { return setAudio(function (audio) { return !audio; }); } }, audio ? React.createElement(React.Fragment, null,
         React.createElement("audio", { ref: streamRef, autoPlay: true, className: 'hidden' }),
         React.createElement(BsFillMicFill, { title: "Turn mic off" })) : React.createElement(BsFillMicMuteFill, { title: "Turn mic on" }));
 }
--- build/useStorage.js
+++ build/useStorage.js
@@ -1,10 +1,33 @@
+var __read = (this && this.__read) || function (o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+};
 import { useState } from "react";
-const setStorage = (key, value, local = false) => (local ? localStorage : sessionStorage).setItem(key, JSON.stringify(value));
-const removeStorage = (key, local = false) => (local ? localStorage : sessionStorage).removeItem(key);
-const getStorage = (key, fallbackValue, local = false) => {
+var setStorage = function (key, value, local) {
+    if (local === void 0) { local = false; }
+    return (local ? localStorage : sessionStorage).setItem(key, JSON.stringify(value));
+};
+var removeStorage = function (key, local) {
+    if (local === void 0) { local = false; }
+    return (local ? localStorage : sessionStorage).removeItem(key);
+};
+var getStorage = function (key, fallbackValue, local) {
+    if (local === void 0) { local = false; }
     if (typeof window === "undefined")
         return fallbackValue;
-    let value = (local ? localStorage : sessionStorage).getItem(key);
+    var value = (local ? localStorage : sessionStorage).getItem(key);
     try {
         if (!value)
             throw new Error("Value doesn't exist");
@@ -22,12 +45,13 @@ const getStorage = (key, fallbackValue, local = false) => {
     }
     return value;
 };
-export default function useStorage(key, initialValue, { local = false, save = false } = {}) {
+export default function useStorage(key, initialValue, _a) {
+    var _b = _a === void 0 ? {} : _a, _c = _b.local, local = _c === void 0 ? false : _c, _d = _b.save, save = _d === void 0 ? false : _d;
     save || (save = getStorage('mode') !== 'online');
-    const [storedValue, setStoredValue] = useState(save ? getStorage(key, initialValue, local) : initialValue);
-    const setValue = (value) => {
-        setStoredValue((old) => {
-            const updatedValue = typeof value === 'function' ? value(old) : value;
+    var _e = __read(useState(save ? getStorage(key, initialValue, local) : initialValue), 2), storedValue = _e[0], setStoredValue = _e[1];
+    var setValue = function (value) {
+        setStoredValue(function (old) {
+            var updatedValue = typeof value === 'function' ? value(old) : value;
             if (save)
                 setStorage(key, updatedValue, local);
             return updatedValue;
