620d46bc08d0dd3afbe0f79db5a28eb7986a5bec
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2023-09-10 00:21:12 +0530

    v0.2.3 tsconfig update

--- build/index.js
+++ build/index.js
@@ -1,28 +1,7 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
-        }
-    return t;
-};
 import React, { useEffect, useRef, useState } from 'react';
 import useStorage, { removeStorage } from './storage';
 import { BiSolidMessageDetail, BiSolidMessageX, BsFillMicFill, BsFillMicMuteFill, GrSend } from './icons';
-export default function Chat(_a) {
-    var { name, peerId, remotePeerId, peerOptions, text = true, voice = true, dialogOptions, onError = () => console.error("Can not access microphone!"), children } = _a, props = __rest(_a, ["name", "peerId", "remotePeerId", "peerOptions", "text", "voice", "dialogOptions", "onError", "children"]);
+export default function Chat({ name, peerId, remotePeerId, peerOptions, text = true, voice = true, dialogOptions, onError = () => console.error("Can not access microphone!"), children, ...props }) {
     const [peer, setPeer] = useState();
     const [opponentName, setOpponentName] = useState();
     const [notification, setNotification] = useState(false);
@@ -38,11 +17,10 @@ export default function Chat(_a) {
     remotePeerId = `rpc-${remotePeerId}`;
     const handleRemoteStream = (remoteStream) => streamRef.current.srcObject = remoteStream;
     function addMessage(message, send = false) {
-        var _a, _b;
         setMessages(old => old.concat(message));
         if (send)
-            (_a = connRef.current) === null || _a === void 0 ? void 0 : _a.send({ type: 'message', message });
-        else if (!((_b = dialogRef.current) === null || _b === void 0 ? void 0 : _b.open))
+            connRef.current?.send({ type: 'message', message });
+        else if (!dialogRef.current?.open)
             setNotification(true);
     }
     function handleConnection(conn, setName = false) {
@@ -69,12 +47,10 @@ export default function Chat(_a) {
             setPeer(undefined);
             return;
         }
-        (function loadPeer() {
-            return __awaiter(this, void 0, void 0, function* () {
-                const Peer = (yield import('peerjs')).default;
-                const peer = new Peer(`rpc-${peerId}`, peerOptions);
-                setPeer(peer);
-            });
+        (async function loadPeer() {
+            const Peer = (await import('peerjs')).default;
+            const peer = new Peer(`rpc-${peerId}`, peerOptions);
+            setPeer(peer);
         })();
     }, [audio]);
     useEffect(() => {
@@ -109,29 +85,27 @@ export default function Chat(_a) {
         });
         peer.on('connection', handleConnection);
         return () => {
-            var _a, _b, _c;
-            (_a = localStream.current) === null || _a === void 0 ? void 0 : _a.getTracks().forEach(track => track.stop());
-            (_b = connRef.current) === null || _b === void 0 ? void 0 : _b.removeAllListeners();
-            (_c = connRef.current) === null || _c === void 0 ? void 0 : _c.close();
-            call === null || call === void 0 ? void 0 : call.removeAllListeners();
-            call === null || call === void 0 ? void 0 : call.close();
+            localStream.current?.getTracks().forEach(track => track.stop());
+            connRef.current?.removeAllListeners();
+            connRef.current?.close();
+            call?.removeAllListeners();
+            call?.close();
             peer.removeAllListeners();
             peer.destroy();
         };
     }, [peer]);
     useEffect(() => {
-        var _a, _b;
         if (dialog)
-            (_a = dialogRef.current) === null || _a === void 0 ? void 0 : _a.show();
+            dialogRef.current?.show();
         else
-            (_b = dialogRef.current) === null || _b === void 0 ? void 0 : _b.close();
+            dialogRef.current?.close();
     }, [dialog]);
     useEffect(() => {
         const container = containerRef.current;
         if (container)
             container.scrollTop = container.scrollHeight;
     }, [dialog, opponentName, messages]);
-    return React.createElement("div", Object.assign({ className: 'rpc-main' }, props),
+    return React.createElement("div", { className: 'rpc-main', ...props },
         typeof children === 'function' ? children({ notification, messages, addMessage, dialogRef, audio, setAudio }) : React.createElement(React.Fragment, null,
             text && React.createElement("div", null,
                 dialog ? React.createElement(BiSolidMessageX, { onClick: () => setDialog(false) })
@@ -141,7 +115,7 @@ export default function Chat(_a) {
                                 setDialog(true);
                             } }),
                         notification && React.createElement("span", { className: 'rpc-badge' })),
-                React.createElement("dialog", { ref: dialogRef, className: `${dialog ? 'dialog' : ''} position-${(dialogOptions === null || dialogOptions === void 0 ? void 0 : dialogOptions.position) || 'center'}`, style: dialogOptions === null || dialogOptions === void 0 ? void 0 : dialogOptions.style },
+                React.createElement("dialog", { ref: dialogRef, className: `${dialog ? 'dialog' : ''} position-${dialogOptions?.position || 'center'}`, style: dialogOptions?.style },
                     React.createElement("div", { className: 'rpc-heading' }, "Chat"),
                     React.createElement("hr", null),
                     React.createElement("div", null,
@@ -152,9 +126,8 @@ export default function Chat(_a) {
                             React.createElement("span", null, text)))),
                         React.createElement("hr", null),
                         React.createElement("form", { className: 'rpc-input-container', onSubmit: e => {
-                                var _a;
                                 e.preventDefault();
-                                const text = (_a = inputRef.current) === null || _a === void 0 ? void 0 : _a.value;
+                                const text = inputRef.current?.value;
                                 if (text) {
                                     inputRef.current.value = '';
                                     addMessage({ id: peerId, text }, true);
