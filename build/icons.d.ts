d69924e9d92b7a691cead3d8f27fdeb845f33d86
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2023-09-22 14:53:49 +0530

    remove build

--- build/icons.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-export type IconProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>;
-export declare function BiSolidMessageDetail(props: IconProps): React.JSX.Element;
-export declare function BiSolidMessageX(props: IconProps): React.JSX.Element;
-export declare function GrSend(props: IconProps): React.JSX.Element;
-export declare function BsFillMicFill(props: IconProps): React.JSX.Element;
-export declare function BsFillMicMuteFill(props: IconProps): React.JSX.Element;
--- build/storage.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const removeStorage: (key: string, local?: boolean) => void;
-export default function useStorage<Value>(key: string, initialValue: Value, { local, save }?: {
-    local?: boolean | undefined;
-    save?: boolean | undefined;
-}): [Value, (value: Value | ((old: Value) => Value)) => void];
